2442d8b5411252c78aa57bdc2e8e18ca97460260
Hayden Sweet <haydensweet22@gmail.com>
2022-04-15 11:15:32 -0600

    Password completed

--- password.js
+++ password.js
@@ -3,4 +3,27 @@ const readline = require('readline');
 const reader = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-}
+});
+
+console.log (String.raw`
+ __      __       .__                               
+/  \    /  \ ____ |  |   ____  ____   _____   ____  
+\   \/\/   // __ \|  | _/ ___\/  _ \ /     \_/ __ \ 
+ \        /\  ___/|  |_\  \__(  <_> )  Y Y  \  ___/ 
+  \__/\  /  \___  >____/\___  >____/|__|_|  /\___  >
+       \/       \/          \/            \/     \/ 
+`);
+
+reader.question("Please enter a password:", function(input){
+	tokens = input.split(' ');
+	
+	password = tokens[0];
+
+    if (password.length >= 10 && password.includes('%') && password.length <= 20) {
+        console.log("Password successful!");
+    } else if (password === "password" || password === "Password") {
+        console.log("That is a terrible password");
+    }  else {console.log("Password failed. Please enter between 10-20 characters with atleast one % sign")};
+
+    reader.close();
+});
